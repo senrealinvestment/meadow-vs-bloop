e6201d915291f94ce4c97388b9434b6eb96baff5
senrealinvestment <senrealinvestment@gmail.com>
2026-08-23 11:59:11 -0400

    Phase 3 World 1: app.loader.js WORLD1_BANK eval loader

---
+++ app.loader.js
@@ -0,0 +1,6 @@
+/*WORLD1_BANK*/
+(function(){
+  var s=(window.__MVB0||"")+(window.__MVB1||"")+(window.__MVB2||"");
+  try{delete window.__MVB0;delete window.__MVB1;delete window.__MVB2;}catch(e){}
+  (0,eval)(s);
+})();
