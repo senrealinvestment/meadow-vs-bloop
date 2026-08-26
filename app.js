e29ea19916f4b3edf50f0f2b36bbafa11d39ff6c
senrealinvestment <senrealinvestment@gmail.com>
2026-08-26 17:56:41 -0400

    fix: embed Frost Path tiles (no CDN miss, never Meadow grass)

--- app.js
+++ app.js
@@ -32,7 +32,7 @@
     return false;
   }
   await tryLoad(base+"asset-boss.js");
-  var embeds=['a0.js','a1.js','a2.js','a3.js','a4.js','a5.js','a6.js','a7.js','a8.js','a9.js','a10.js','a11.js','a12.js','a13.js','a14.js','a15.js','a16.js','a17.js','a18.js','a19.js','a20.js','a21.js','a22.js','a23.js','a24.js','a25.js','a26.js','embed-tiles.js','embed-npcs.js'];
+  var embeds=['a0.js','a1.js','a2.js','a3.js','a4.js','a5.js','a6.js','a7.js','a8.js','a9.js','a10.js','a11.js','a12.js','a13.js','a14.js','a15.js','a16.js','a17.js','a18.js','a19.js','a20.js','a21.js','a22.js','a23.js','a24.js','a25.js','a26.js','embed-tiles.js','embed-frost-tiles.js','embed-npcs.js'];
   for(var i=0;i<embeds.length;i++) await tryLoad(base+embeds[i]);
   var overrides=['embed-walk.js','embed-cast.js','embed-foes.js'];
   for(var oi=0;oi<overrides.length;oi++) await tryLoad(base+overrides[oi]);
