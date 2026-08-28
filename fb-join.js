a740cbfc692925e07edd7499527a4769fea4b278
senrealinvestment <senrealinvestment@gmail.com>
2026-08-27 22:42:09 -0400

    Add fb-join.js full-body PNG embed assembler

---
+++ fb-join.js
@@ -0,0 +1,7 @@
+window.MEADOW_ASSET_EMBED=window.MEADOW_ASSET_EMBED||{};
+window.MEADOW_ASSET_EMBED["foes/bloop-fluff-sheet.png"]="data:image/png;base64,"+(window.__MVB_FB0+window.__MVB_FB1+window.__MVB_FB2+window.__MVB_FB3+window.__MVB_FB4);
+window.MEADOW_ASSET_EMBED["assets/foes/bloop-fluff-sheet.png"]="data:image/png;base64,"+(window.__MVB_FB0+window.__MVB_FB1+window.__MVB_FB2+window.__MVB_FB3+window.__MVB_FB4);
+window.MEADOW_ASSET_EMBED["foes/frost-bloop-fluff-sheet.png"]="data:image/png;base64,"+(window.__MVB_FFB0+window.__MVB_FFB1+window.__MVB_FFB2+window.__MVB_FFB3+window.__MVB_FFB4);
+window.MEADOW_ASSET_EMBED["assets/foes/frost-bloop-fluff-sheet.png"]="data:image/png;base64,"+(window.__MVB_FFB0+window.__MVB_FFB1+window.__MVB_FFB2+window.__MVB_FFB3+window.__MVB_FFB4);
+window.MEADOW_ASSET_EMBED["bosses/ice_howl.png"]="data:image/png;base64,"+(window.__MVB_IHB0+window.__MVB_IHB1+window.__MVB_IHB2+window.__MVB_IHB3+window.__MVB_IHB4);
+window.MEADOW_ASSET_EMBED["assets/bosses/ice_howl.png"]="data:image/png;base64,"+(window.__MVB_IHB0+window.__MVB_IHB1+window.__MVB_IHB2+window.__MVB_IHB3+window.__MVB_IHB4);
